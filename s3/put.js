571e4e14f5625a5d2c578411797ef37879b7d81a
bmurdock <S81ltr#m0p>
2020-12-07 17:07:32 -0700

    updated s3 settings

--- s3/put.js
+++ s3/put.js
@@ -6,7 +6,7 @@ module.exports.put = (event, context, callback) => {
     console.log('event: ', event);
     console.log('env: ', process.env);
 
-    fs.readFile('../starterlist.json', 'utf8', (err, data) =>
+    fs.readFile('./starterlist.json', 'utf8', (err, data) =>
     {
         if (err)
         {
@@ -19,6 +19,7 @@ module.exports.put = (event, context, callback) => {
             Bucket: process.env.BUCKET,
             Key: 'test.json',
             Body: data,
+            ContentType: 'application/json; charset=utf-8',
         },function(err, data) {
             if (err) callback(err, err.stack); // an error occurred
             else callback(null,data); // successful response
@@ -30,24 +31,5 @@ module.exports.put = (event, context, callback) => {
             */
         });
 
-    })
-    callback(null, {});
-    return;
-	console.log('bucket: ', process.env.S3_BUCKET);
-
-	var params = {
-		Body: '',
-		Bucket: process.env.S3_BUCKET,
-		Key: 'test.json'
-	};
-	s3.putObject(params, function(err, data) {
-		if (err) callback(err, err.stack); // an error occurred
-		else callback(null,data); // successful response
-		/*
-		data = {
-		 ETag: "\"6805f2cfc46c0f04559748bb039d69ae\"", 
-		 VersionId: "tpf3zF08nBplQK1XLOefGskR7mGDwcDk"
-		}
-		*/
-	});
+    });
 }
